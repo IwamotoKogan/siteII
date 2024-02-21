53658cf667f73dcca945d92efff7473a7dd5e13c
IwamotoKogan <141932936+IwamotoKogan@users.noreply.github.com>
2024-02-21 14:46:36 +0100

    Update popup3.js

--- js/popup3.js
+++ js/popup3.js
@@ -33,6 +33,7 @@ if (savedItems.length > 0) {
             <td class="product-name ">
                 <h2 class="h5 text-black">Pozicija šarki? ${item.hinges}</h2>
                 <h2 class="h5 text-black">Potrebne nogice? ${item.answer}</h2>
+                <h2 class="h5 text-black">Potrebne nogice? ${item.korpus}</h2>
             </td>
             <td class="product-name ">cena vašeg elementa je<strong> ${item.price}</strong>  evra</td>
                
