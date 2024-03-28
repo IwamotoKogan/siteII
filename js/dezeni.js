a58702f727359805fd442409dce282a5d9c56da8
IwamotoKogan <141932936+IwamotoKogan@users.noreply.github.com>
2024-03-28 17:48:56 +0100

    Update dezeni.js

--- js/dezeni.js
+++ js/dezeni.js
@@ -395,7 +395,7 @@ const korpusOdgovor = crni.classList.contains('selektovan') ? 'crni' : beli.clas
 
         // Redirektuj na stranicu pregled_kuhinja.html
 	alert("Uspešno ste kreirali element.");
-	    showNotification();
+	    
        location.reload();
     }
 }
