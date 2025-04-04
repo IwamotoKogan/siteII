c5f814c9135c540c6a87bba1cdb4964d705b3efb
IwamotoKogan <141932936+IwamotoKogan@users.noreply.github.com>
2025-04-04 22:45:52 +0200

    Update korpus_2OCPAF_102.js

--- js/korpus_2OCPAF_102.js
+++ js/korpus_2OCPAF_102.js
@@ -17,7 +17,7 @@ let dezeni = [];
 
 let imeKanta = "Nijedna";
 let kantTrake = [];
-let izabraniLesonit = null;
+let izabraniLesonit = 0;
 console.log("Izabrani lesonit:", izabraniLesonit);
 let imeLesonita = "";
 
@@ -536,15 +536,13 @@ function addToCart(dezeni) {
 
 
 
-   /*if (!(isCrniKorpus || isBeliKorpus || isSiviKorpus)) {
-       alert("Odaberite korpus");
-       return;
-   }*/
-
-   if (izabraniLesonit === null) {
+   /*if (izabraniLesonit === null) {
     alert("Niste odabrali lesonit.");
     return;
 }
+   }*/
+
+   
  
    if (!(isYesSelected || isNoSelected)) {
        alert("Niste odgovorili da li želite da element ima ugradjene nogice'.");
